38e14341690632afcdff9c7fd8998bb6b5c092a3
Yasir <yasir123119@gmail.com>
2020-11-24 21:38:53 +0530

    first commit with Read me

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # scrapy
-this is a webcrawler
+this is an advanced webcrawler for specific tasks.
+
+the script has a class called **Crawl** which takes the following parameters:
+1. url : the url which to crawl
+2. path : optional list to be provided where each index value is a key required in each level
+    Eg: path = ["abc", "xyz"]
+    this means during BFS crawling at level=0 this will require "abc" key present in the url to consider it for further crawling.
+3. imp_key : optional string value - this is simialar to above one, but this is required in any level
---
+++ scarpy.py
@@ -0,0 +1,151 @@
+import os
+import requests
+import requests.exceptions
+from urllib.parse import urlsplit
+from urllib.parse import urlparse
+from collections import deque
+from bs4 import BeautifulSoup
+
+
+
+class Crawl():
+    def __init__(self, url, path=None, imp_key=None):
+        self.url = url
+        self.path = path
+        self.imp_key = imp_key
+        #url = 'https://www.mayoclinic.org/diseases-conditions'
+        # a queue of urls to be crawled next
+        self.new_urls = deque([(url,0)])
+        # a set of urls that we have already processed
+        self.processed_urls = set()
+        # a set of domains inside the target website
+        self.local_urls = set()
+        # a set of domains outside the target website
+        self.foreign_urls = set()
+        # a set of broken urls
+        self.broken_urls = set()
+        # a set of unwanted links
+        self.unwanted_urls = set()
+        # define log files
+        self.delete_logs()
+        self.init_logs()
+    
+    def init_logs(self):
+        self.final_url_log = open("final_url_log.txt", "w+")
+        self.all_url_log = open("all_url_log.txt", "w+")
+        self.test_log = open("test_log.txt", "w+")
+        self.log = open("log.txt", "w+")
+
+    def close_logs(self):
+        self.final_url_log.close()
+        self.all_url_log.close()
+        self.test_log.close()
+        self.log.close()
+    
+    def delete_logs(self):
+        cmd = "rm -rf *txt"
+        os.system(cmd)
+
+    def save_data(self, data, file):
+        with open(file, "w+") as fp:
+            for url in data:
+                fp.write("{}\n".format(url))
+    
+    def bfs_level_data_log(self, data, level):
+        filename = "level_{}_contents.txt".format(level)
+        with open(filename, "a+") as fp:
+            fp.write(data)
+
+    def bfs_url_crawl(self, level=3):
+        url_count = 0
+        stop_flag = 0
+        # process urls one by one until we exhaust the queue
+        while len(self.new_urls) and not stop_flag:
+            if len(self.new_urls) > 5000:
+                stop_flag = 1
+            # move url from the queue to processed url set
+            pop_val = self.new_urls.popleft()
+            url, cur_level = pop_val
+            info = "url : {} - Depth level : {}\n".format(url, cur_level)
+            self.all_url_log.write(info)
+            self.processed_urls.add(url)
+
+            url_count += 1
+            if url_count%10 == 0:
+                print("======= {} url's Proccessed ====== ".format(url_count))
+                print("number of new urls in Que : {}".format(len(self.new_urls)))
+                for i, url in enumerate(self.new_urls):
+                    if i < url_count - 10:
+                        continue
+                    self.log.write("{}\n".format(url))
+
+            
+            if cur_level > level:
+                print("skipping url : {}  <> cur_level : {}".format(url, cur_level))
+                continue
+            
+            try:
+                response = requests.get(url)
+            except(
+                requests.exceptions.MissingSchema, requests.exceptions.ConnectionError,
+                requests.exceptions.InvalidURL, requests.exceptions.InvalidSchema
+                ):
+                # add broken urls to it’s own set, then continue
+                self.broken_urls.add(url)
+                continue
+            # extract base url to resolve relative links
+            parts = urlsplit(url)
+            # base = "{0.netloc}".format(parts)
+            # strip_base = base.replace("www.", "")
+            base_url = "{0.scheme}://{0.netloc}".format(parts)
+            # path = url[:url.rfind('/')+1] if '/' in parts.path else url
+
+            soup = BeautifulSoup(response.text, "lxml")
+
+            for link in soup.find_all('a'):
+                # extract link url from the anchor
+                anchor = link.attrs["href"] if "href" in link.attrs else ''
+                
+                local_link = base_url + anchor
+                if local_link in self.local_urls or anchor.startswith('/es-es') or anchor.startswith('/ar/'):
+                    continue
+
+                strict_flag = False
+                if cur_level < len(self.path):
+                    strict_flag = self.path[cur_level] in anchor
+                elif self.imp_key in anchor:
+                    strict_flag = True
+                
+                end_flag = anchor.endswith("?p=1") if cur_level >= 2 else True
+
+                info = "url : {} - Depth level : {}\n".format(local_link, cur_level)
+                self.bfs_level_data_log(info, cur_level)
+
+                if anchor.startswith('/') and strict_flag and end_flag:
+                    local_link = base_url + anchor
+                    self.local_urls.add(local_link)
+                    self.final_url_log.write("url : {} - Depth level : {}\n".format(local_link, cur_level))
+                # elif strip_base in anchor:
+                #     self.unwanted_urls.add(anchor)
+                # elif not anchor.startswith('http'):
+                #     local_link = path + anchor
+                #     self.unwanted_urls.add(local_link)
+                # else:
+                #     self.foreign_urls.add(anchor)
+                if not stop_flag:
+                    for i in self.local_urls:
+                        #for using only local
+                        if not (i, cur_level+1) in self.new_urls and not i in self.processed_urls:
+                            self.new_urls.append((i, cur_level+1))
+                #for using all the urls
+                # if not link in self.new_urls and not link in self.processed_urls:
+                #     self.new_urls.append(link)
+        self.save_data(self.local_urls, "valid_url_list.txt")
+        # self.save_data(self.unwanted_urls, "invalid_url_list.txt")
+
+url = 'https://www.mayoclinic.org/diseases-conditions'
+path_level = ["index","diseases-conditions"]
+must_have_key = "diseases-conditions"
+obj = Crawl(url, path_level, must_have_key)
+obj.bfs_url_crawl(level=3)
+obj.close_logs()
